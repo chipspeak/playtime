5a9f0a674005d303ec57c3bdb6a4c4270549efff
Pat Walsh-O'Connor <114754356+chipspeak@users.noreply.github.com>
2024-01-25 20:11:33 +0000

    Pre-validation
    Data is only stored per session and is lost upon closing application.
    - Deletion of playlist and tracks functioning
    - Sessions functioning
    
    Next objective is implementation of validation and JSON reading/writing

---
+++ src/models/mem/playlist-mem-store.js
@@ -0,0 +1,52 @@
+import { v4 } from "uuid";
+import { trackMemStore } from "./track-mem-store.js";
+
+let playlists = [];
+
+export const playlistMemStore = {
+  async getAllPlaylists() {
+    return playlists;
+  },
+
+  async addPlaylist(playlist) {
+    playlist._id = v4();
+    playlists.push(playlist);
+    return playlist;
+  },
+
+  async getPlaylistById(id) {
+    const list = playlists.find((playlist) => playlist._id === id);
+    list.tracks = await trackMemStore.getTracksByPlaylistId(list._id);
+    return list;
+  },
+
+  async deletePlaylistById(id) {
+    const index = playlists.findIndex((playlist) => playlist._id === id);
+    playlists.splice(index, 1);
+  },
+
+  async deleteAllPlaylists() {
+    playlists = [];
+  },
+
+  
+  async addTrackToPlaylist(playlistId, track) {
+    const playlist = await this.getPlaylistById(playlistId);
+    if (!playlist) {
+      throw new Error("Playlist not found");
+    }
+
+    track._id = v4();
+    track.playlistId = playlistId;
+
+    playlist.tracks = playlist.tracks || [];
+    playlist.tracks.push(track);
+
+    return track;
+  },
+
+  async getUserPlaylists(userid) {
+    return playlists.filter((playlist) => playlist.userid === userid);
+  },
+
+};
---
+++ src/server.js
@@ -0,0 +1,58 @@
+import Hapi from "@hapi/hapi";
+import Vision from "@hapi/vision";
+import Handlebars from "handlebars";
+import path from "path";
+import dotenv from "dotenv"; 
+import { fileURLToPath } from "url";
+import Cookie from "@hapi/cookie";
+import { webRoutes } from "./web-routes.js";
+import { db } from "./models/db.js";
+import { accountsController } from "./controllers/accounts-controller.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+
+async function init() {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  });
+  await server.register(Vision);
+  await server.register(Cookie);
+  server.views({
+    engines: {
+      hbs: Handlebars,
+    },
+    relativeTo: __dirname,
+    path: "./views",
+    layoutPath: "./views/layouts",
+    partialsPath: "./views/partials",
+    layout: true,
+    isCached: false,
+  });
+  db.init();
+  server.route(webRoutes);
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+
+  server.auth.strategy("session", "cookie", {
+    cookie: {
+      name: process.env.COOKIE_NAME,
+      password: process.env.COOKIE_PASSWORD,
+      isSecure: false,
+    },
+    redirectTo: "/",
+    validate: accountsController.validate,
+  });
+  server.auth.default("session");
+  
+}
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
